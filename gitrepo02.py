1d9ec236a0151d8332c5a7069bd6641201ae195a
Shwetabhirad <shweta.bhirad55@gmail.com>
2023-07-06 23:14:06 +0530

    this is my second repository

---
+++ gitrepo02.py
@@ -0,0 +1,10 @@
+num1 = 20
+num2 = 10
+num3 = 50
+
+if num1>=num2 and num2>=num3 :
+    print("num1 is greater:", num1)
+elif num2>=num3 and num2>=num1 :
+    print("num2 is greater:", num2)
+else:
+    print("num3 is greater",num3)
